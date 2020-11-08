afebfcbf7786f90442636d25a86efa30fec8bead
chiefblackhawk <53925183+chiefblackhawk@users.noreply.github.com>
2020-11-08 07:43:47 -0800

    Update calculator.py
    Minor changes to add exception handling for user input on number values, making sure program doesn't fail when user accidentally or maliciously enters in a non-integer value.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-#Calculator v1.0 that allows for two integers to execute a few arithmetic methods such as addition, subratction, and so forth...
+#Calculator v1.1 that allows for two integers to execute a few arithmetic methods such as addition, subratction, and so forth...
+
 class Calculator():
-	"""docstring for Calculator"""
 	def __init__(self, num1, num2):
 		super(Calculator, self).__init__()
 		self.num1 = num1
@@ -21,25 +21,33 @@ class Calculator():
 
 def main():
 	print("Welcome to the Calculator")
+	num_list = []
+	
 	On = True
 	while On:
-		number1 = int(input("Please enter your first number: "))
-		number2 = int(input("Please enter your second number: "))
-		my_instance = Calculator(number1, number2)
-
-		function = input("Enter 'a' for addition, 's' for subtraction, 'm' for multiplication, 'd' for division, or 'q' to quit.")
-		if function in ['addition', 'ADDITION', 'a', 'A', 'add', 'Add', 'ADD']:
-			my_instance.addition()
-		elif function in ['subtraction','SUBTRACTION','s','S','subtract','Subtract','SUBTRACT']:
-			my_instance.subtraction()
-		elif function in ['multiplication','MULTIPLICATION','m','M','multiply','Multiply','MULTIPLY']:
-			my_instance.multiplication()
-		elif function in ['division','DIVISION','d','D','divide','Divide','DIVIDE']:
-			my_instance.division()
-		elif function in ['quit','QUIT','q','Q']:
-			print("Quiting calculator program...")
-			break
+		try:
+			number1 = int(input("Please enter your first number: "))
+			number2 = int(input("Please enter your second number: "))
+		except ValueError:
+			print("You must use an integer value in the calculator.")
+			continue
 		else:
-			print("Invalid entry...")
+			my_instance = Calculator(number1, number2)
+
+			function = input("Enter 'a' for addition, 's' for subtraction, 'm' for multiplication, 'd' for division, or 'q' to quit.")
+			if function in ['addition', 'ADDITION', 'a', 'A', 'add', 'Add', 'ADD']:
+				my_instance.addition()
+			elif function in ['subtraction','SUBTRACTION','s','S','subtract','Subtract','SUBTRACT']:
+				my_instance.subtraction()
+			elif function in ['multiplication','MULTIPLICATION','m','M','multiply','Multiply','MULTIPLY']:
+				my_instance.multiplication()
+			elif function in ['division','DIVISION','d','D','divide','Divide','DIVIDE']:
+				my_instance.division()
+			elif function in ['quit','QUIT','q','Q']:
+				print("Quiting calculator program...")
+				break
+			else:
+				print("Invalid entry...")
+
 if __name__ == '__main__':
 	main()
